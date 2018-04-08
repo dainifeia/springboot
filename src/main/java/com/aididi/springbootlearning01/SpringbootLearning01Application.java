a4834f363151dc24798a217ad922961594ed8cf3
xuzexiang <756357181@qq.com>
2018-04-08 18:39:25 +0800

    添加了一些配置文件

--- src/main/java/com/aididi/springbootlearning01/SpringbootLearning01Application.java
+++ src/main/java/com/aididi/springbootlearning01/SpringbootLearning01Application.java
@@ -2,11 +2,24 @@ package com.aididi.springbootlearning01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+@RestController
 @SpringBootApplication
 public class SpringbootLearning01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootLearning01Application.class, args);
+	}
+
+	@RequestMapping(value = "/" ,produces = "text/plain;charset=UTF-8")
+	Map index(){
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("a",1);
+		map.put("b",2);
+		return map;
 	}
 }
