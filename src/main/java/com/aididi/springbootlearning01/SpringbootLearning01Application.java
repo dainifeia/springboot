868acc27b1f5aa15b87104a11e05fe0fb10fb513
xuzexiang <756357181@qq.com>
2018-04-08 10:18:40 +0800

    第一次提交

---
+++ src/main/java/com/aididi/springbootlearning01/SpringbootLearning01Application.java
@@ -0,0 +1,12 @@
+package com.aididi.springbootlearning01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearning01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearning01Application.class, args);
+	}
+}
