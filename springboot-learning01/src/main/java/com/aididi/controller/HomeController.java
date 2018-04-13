cde1e7f6c3f0152f2e7a9884fc73dc411fb6c568
xuzexiang <756357181@qq.com>
2018-04-13 15:17:41 +0800

    初次提交

---
+++ springboot-learning01/src/main/java/com/aididi/controller/HomeController.java
@@ -0,0 +1,5 @@
+package com.aididi.controller;
+
+public class HomeController {
+
+}
---
+++ springboot-learning02/src/main/java/com/aididi/SpringbootLearning02Application.java
@@ -0,0 +1,12 @@
+package com.aididi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearning02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearning02Application.class, args);
+	}
+}
